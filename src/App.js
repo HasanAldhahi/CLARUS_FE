a49528d31c837ed3be103f36d29611f89e8e5e6e
nbbhk <hassanshka@gmail.com>
2023-09-14 15:42:44 +0200

    create the repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Button from "./Components/Button"
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="Hello">  </Button>
+      <div className="bg-blue-500 text-white p-4">
+        This is a test element with Tailwind CSS styles.
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ text }) => {
+  return (
+    <button className="bg-blue-500 ">
+      {text}
+    </button>
+  );
+};
+
+export default Button;
